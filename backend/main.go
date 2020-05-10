b2fe77a561e9dc13efff7f9c3d4f1cf98e0757f2
CatchZeng <catchzenghh@gmail.com>
2020-05-10 10:32:15 +0800

    feat: fix sed error

--- backend/main.go
+++ backend/main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/appboot/appboot/utils"
 	"net/http"
 
 	"github.com/appboot/appboot/handler"
-	"github.com/appboot/appboot/utils"
 	"golang.org/x/net/websocket"
 )
 
 func main() {
-	fmt.Printf("IP: %v", utils.GetIP())
-	http.Handle("/ws", websocket.Handler(handler.Handle))
-	err := http.ListenAndServe(":8888", nil)
+	const port = ":8888"
+	const pattern = "/appboot"
+	url := fmt.Sprintf("ws://%v:%v/%v",utils.GetIP(),port,pattern)
+	fmt.Printf("WS_URL: %v", url)
+
+	http.Handle(pattern, websocket.Handler(handler.Handle))
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
--- frontend/start.sh
+++ frontend/start.sh
@@ -1,8 +1,9 @@
 cd /app/js
 JSPATH=`find . -name 'app.*.js'`
 JSNAME=`echo ${JSPATH#*/}`
-SOURCE_WS_URL="ws://ws.appboot.com:8888/ws"
-sed -i "s/$SOURCE_WS_URL/$WS_URL/g" $JSNAME
+SOURCE_WS_URL="ws://ws.appboot.com:8888/appboot"
+# https://clubmate.fi/replace-strings-in-files-with-the-sed-bash-command/
+sed -i "s|${SOURCE_WS_URL}|${WS_URL}|g" $JSNAME
 
 cd /app
 nginx -g 'daemon off;'
